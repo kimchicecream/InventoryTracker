2141b50efbfbe0ec06798c0f1eb9ade750cb4a81
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-02-28 19:41:34 -0700

    delected in_stock column and added link column

--- backend/models.py
+++ backend/models.py
@@ -4,10 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Part(Base):
-    __Tablename__ = "parts"
+    __tablename__ = "parts"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     quantity = Column(Integer, default = 0)
-    price = Column(Float, nullable = False)
-    in_stock = Column(Boolean, default=True)
+    price = Column(Float, nullable=False)
+    link = Column(String)

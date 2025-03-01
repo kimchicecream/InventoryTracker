d3cd62826e78ccf08577e6528ff193e417063b84
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-03-01 01:01:02 -0700

    initiated Homepage component

---
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import './Homepage.css';
+
+function Homepage() {
+    return (
+        <div className='homepage'>
+            <h1>Test</h1>
+        </div>
+    );
+}
+
+export default Homepage;

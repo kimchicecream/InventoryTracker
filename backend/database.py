3cfb746cd168ef874d1e05ba5aa199ab6b8d626a
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-03-14 02:56:05 -0700

    configured database.py to persist data via disk

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from backend.models import Base
 
-DATABASE_URL = "sqlite:////app/app.db"
+DATABASE_URL = "sqlite:////data/app.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

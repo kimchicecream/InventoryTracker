765580ce025fd05444f27582ecd6c3ac1fca0d5d
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-04-12 11:03:47 -0700

    added dynamic base in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/static/',
+  base: isProduction ? '/static/' : '/',
   plugins: [react()],
   build: {
     outDir: path.resolve(__dirname, '../backend/static'),
